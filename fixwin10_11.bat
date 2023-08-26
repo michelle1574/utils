1385cf63e6ff6d60c33c22d289bf63592f535d1c
Michael <70703680+michelle1574@users.noreply.github.com>
2023-08-26 02:47:04 +0200

    Update fixwin10_11.bat

--- fixwin10_11.bat
+++ fixwin10_11.bat
@@ -1,6 +1,21 @@
 @echo off
-sfc /scannow
-DISM /Online /Cleanup-Image /CheckHealth
-DISM /Online /Cleanup-Image /ScanHealth
-DISM /Online /Cleanup-Image /RestoreHealth
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Administrator permissions required. Restarting script with admin privileges...
+    goto UACPrompt
+) else ( goto Continue )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:Continue
+    sfc /scannow
+    DISM /Online /Cleanup-Image /CheckHealth
+    DISM /Online /Cleanup-Image /ScanHealth
+    DISM /Online /Cleanup-Image /RestoreHealth
+
 exit
